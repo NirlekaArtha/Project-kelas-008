e48b0016cb03738c9649597cd1e1270cc037594e
M.Faiz <luthfimuladi24@gmail.com>
2025-04-15 11:36:43 +0700

    init

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ function App() {
       <div className="h-screen flex justify-center items-center">
         <div>
           <h1>woyy</h1>
+          <h2>test</h2>
         </div>
       </div>
     </>
