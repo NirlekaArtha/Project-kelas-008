0723b786a9cde5168b9fa0fb22b0356d8d86ec7e
M.Faiz <luthfimuladi24@gmail.com>
2025-04-15 11:32:50 +0700

    ngetest

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="h-screen flex justify-center items-center">
+        <div>
+          <h1>woyy</h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
