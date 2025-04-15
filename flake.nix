9cebb27f48d6bc041bc76ec71680181468fc3c85
myamusashi <myamusashi@proton.me>
2025-04-15 13:26:16 +0700

    push file environment buat gua doang

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  }: let
+    supportedSystems = ["x86_64-linux" "aarch64-linux"];
+    forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+  in {
+    devShell = forAllSystems (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+        pkgs.mkShell {
+          name = "python_dev";
+          buildInputs = with pkgs; [
+            nodejs
+          ];
+        }
+    );
+  };
+}
