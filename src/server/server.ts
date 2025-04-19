87e67467fd599ef8490bab596c667647e1b7ae6f
myamusashi <myamusashi@proton.me>
2025-04-19 12:42:01 +0700

    fix: benerin dev:server buat jalanin server

--- src/server/server.ts
+++ src/server/server.ts
@@ -7,6 +7,11 @@ app.get('/', (req,res)=> {
 	res.send(`Server running in ${config.env} mode`);
 });
 
+app.get("/api", (req,res,next) => {
+	res.json({title: "04tplp008"});
+	next();
+})
+
 app.listen(config.port, () => {
 	console.log(`Server started on ${config.port}`)
 	console.log(`Env: ${config.env}`)
