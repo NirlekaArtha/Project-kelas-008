5ff8b0ae43b970d0ba18c6569d8003a0950c8279
myamusashi <myamusashi@proton.me>
2025-04-20 15:37:48 +0700

    hapus double package.json

--- src/server/config/index.ts
+++ src/server/config/index.ts
@@ -1,9 +1,6 @@
 import dotenv from 'dotenv'
-import path from 'path'
 
-const envPath = path.resolve(
-	__dirname, process.env.NODE_ENV == 'production' ? '../../../.env.production' : '../../../.env'
-);
+const envPath = process.env.PORT;
 
 dotenv.config({path: envPath});
 
