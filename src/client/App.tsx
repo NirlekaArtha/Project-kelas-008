76596f1f8c209c5fab4e793d2b6cbe8034672bcf
M.Faiz <luthfimuladi24@gmail.com>
2025-04-26 22:18:59 +0700

    tampilan authpage sementara

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,12 +1,10 @@
+import LoginPage from "./pages/LoginPage";
+
 function App() {
   return (
     <>
-      <div className="h-screen flex justify-center items-center bg-gray-600">
-        <div>
-          <h1>woyy</h1>
-          <h1>Aloooo</h1>
-          <p>Apis Bau</p>
-        </div>
+      <div>
+        <LoginPage />
       </div>
     </>
   );
